677bd3cf02d387f268be4f37a69870d72cef2d34
joseph78722 <64669439+joseph78722@users.noreply.github.com>
2021-01-03 15:41:13 -0400

    actualizacion, 15min
    sessionInfo:
    R version 4.0.2 (2020-06-22)
    Platform: x86_64-apple-darwin17.0 (64-bit)
    Running under: macOS  10.16
    
    Matrix products: default
    LAPACK: /Library/Frameworks/R.framework/Versions/4.0/Resources/lib/libRlapack.dylib
    
    locale:
    [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
    
    attached base packages:
    [1] stats     graphics  grDevices utils     datasets  methods   base     
    
    other attached packages:
     [1] git2r_0.27.1      htmlwidgets_1.5.2 rvest_0.3.6       xml2_1.3.2       
     [5] forcats_0.5.0     stringr_1.4.0     dplyr_1.0.0       purrr_0.3.4      
     [9] readr_1.3.1       tidyr_1.1.0       tibble_3.0.3      ggplot2_3.3.2    
    [13] tidyverse_1.3.0   leaflet_2.0.3     jsonlite_1.7.2    httr_1.4.2       
    
    loaded via a namespace (and not attached):
     [1] tinytex_0.26      tidyselect_1.1.0  xfun_0.18         haven_2.3.1      
     [5] colorspace_1.4-1  vctrs_0.3.2       generics_0.0.2    htmltools_0.5.0  
     [9] yaml_2.2.1        blob_1.2.1        rlang_0.4.7       pillar_1.4.6     
    [13] withr_2.3.0       glue_1.4.2        DBI_1.1.0         selectr_0.4-2    
    [17] dbplyr_1.4.4      modelr_0.1.8      readxl_1.3.1      lifecycle_0.2.0  
    [21] munsell_0.5.0     gtable_0.3.0      cellranger_1.1.0  evaluate_0.14    
    [25] knitr_1.30        crosstalk_1.1.0.1 curl_4.3          fansi_0.4.1      
    [29] broom_0.7.0       Rcpp_1.0.5        backports_1.1.10  scales_1.1.1     
    [33] fs_1.4.2          hms_0.5.3         packrat_0.5.0     digest_0.6.25    
    [37] stringi_1.5.3     grid_4.0.2        cli_2.0.2         tools_4.0.2      
    [41] magrittr_1.5      crayon_1.3.4      pkgconfig_2.0.3   ellipsis_0.3.1   
    [45] reprex_0.3.0      lubridate_1.7.9.2 assertthat_0.2.1  rmarkdown_2.4    
    [49] rstudioapi_0.11   R6_2.4.1          compiler_4.0.2

---
+++ docs/codigo vuelos.R
@@ -0,0 +1,38 @@
+setwd("~/Vuelos2/docs")
+
+library(httr)
+library(jsonlite)
+library(leaflet)
+library(tidyverse)
+library(xml2)
+library(rvest)
+library(htmlwidgets)
+
+url = "https://opensky-network.org/api/states/all"
+datos <- GET(url)
+datos
+
+datos <- fromJSON(content(datos, type = "text"))
+datos <- datos[["states"]]
+datos
+
+# Extraer datos de columnas para saber qué es cada cosa
+url = "https://opensky-network.org/apidoc/rest.html"
+nombres <- read_html(url) %>% html_nodes("#all-state-vectors")  %>% html_nodes("#response")  %>% html_nodes('.docutils') %>% html_table()
+colnames(datos) <- nombres[[2]]$Property
+datos <- as.data.frame(datos, stringsAsFactors = FALSE)
+
+
+# Convertirmos la latitud y longitud en número, ya que vienen en string (texto)
+datos$longitude <- as.numeric(datos$longitude)
+datos$latitude <- as.numeric(datos$latitude)
+
+# Creamos el mapa
+mapa <- leaflet() %>%
+  addTiles() %>%
+  addCircles(lng = datos$longitude, lat = datos$latitude, color = "#1e3799", opacity = 0.3) %>% 
+  addLegend("topright", colors = "#O3F", labels = "Monitoreo de vuelos") %>% 
+  addLegend("bottomright", colors = "#O3F", labels = "API:opensky-network.org -- Elaborado por J. Romero")
+
+
+saveWidget(mapa, "index.html", selfcontained = F, libdir = "lib")
